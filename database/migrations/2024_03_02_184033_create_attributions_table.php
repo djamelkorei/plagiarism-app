011ed539eb1e91474cf9f629085abc2788e678c3
djamelkorei <korei.djamel.eddine@gmail.com>
2024-03-03 09:48:28 +0000

    update attribution set unique

--- database/migrations/2024_03_02_184033_create_attributions_table.php
+++ database/migrations/2024_03_02_184033_create_attributions_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->integer('credit')->default(3);
             $table->timestamps();
             $table->softDeletes();
+            $table->unique(['reference', 'account_id']);
         });
     }
 
